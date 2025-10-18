0336f03549696156ad4725f807108bbce6a8b4d2
MichaelETU003327 <mikermb18@gmail.com>
2025-10-18 09:33:26 +0300

    decomment

---
+++ src/main/java/com/itu/gest_emp/controller/com/itu/gest_emp/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.itu.gest_emp.controller;
+
+import servlet.annotations.Url;
+
+public class HelloController{
+
+    @Url("/hello")
+    public String hello() {
+        return "Bonjour depuis le framework !";
+    }
+
+    @Url("/bye")
+    public String bye() {
+        return "Au revoir !";
+    }
+
+
+}
