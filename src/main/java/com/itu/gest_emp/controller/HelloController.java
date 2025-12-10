8cf983207f7c4b9c31f1d7232ba889485adadc53
MichaelETU003327 <mikermb18@gmail.com>
2025-12-10 08:08:53 +0300

    MAJ

--- src/main/java/com/itu/gest_emp/controller/HelloController.java
+++ src/main/java/com/itu/gest_emp/controller/HelloController.java
@@ -2,41 +2,38 @@ package com.itu.gest_emp.controller;
 
 import servlet.annotations.Controller;
 import servlet.annotations.RequestParam;
-import servlet.annotations.Url;
 import servlet.annotations.mapping.GetMapping;
 import servlet.annotations.mapping.PostMapping;
 
 @Controller
 public class HelloController {
 
-    @Url("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "Bonjour depuis le framework !";
     }
 
-    @Url("/bye")
+    @GetMapping("/bye")
     public String bye() {
         return "Au revoir !";
     }
 
-    @Url("/etudiant/{id}")
+    @GetMapping("/etudiant/{id}")
     public int get(int id) {
         return id;
     }
 
-    @Url("/personne")
-    @GetMapping
+    @GetMapping("/personne")
     public String get(String var2, Integer id) {
         return var2;
     }
 
-    @Url("/personne")
-    @PostMapping
+    @PostMapping("/personne")
     public String post(String var2, Integer id) {
         return var2;
     }
 
-    @Url("/test/{id}")
+    @GetMapping("/test/{id}")
     public String getVar2(@RequestParam(name = "var2") String var2, Integer id) {
         return var2;
     }
